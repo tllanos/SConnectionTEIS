b49c8fa576faa4062f93668c4feafa69be2add31
Tomás Llano <tllano11@gmail.com>
2016-11-08 12:02:44 -0500

    Initial commit (Fixed)

---
+++ app/src/main/java/co/edu/eafit/teis/sconnection/LoginView.java
@@ -0,0 +1,13 @@
+package co.edu.eafit.teis.sconnection;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_view);
+    }
+}
