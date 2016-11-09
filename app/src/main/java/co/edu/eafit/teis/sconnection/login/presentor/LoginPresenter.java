8f5abe72721905b10409a7953771e0b22949aec0
pachabeche <pachabeche@hotmail.com>
2016-11-08 20:31:47 -0500

    Merge remote-tracking branch 'remotes/origin/signup'
    # Conflicts:
    #	.idea/modules.xml

--- app/src/main/java/co/edu/eafit/teis/sconnection/login/presentor/LoginPresenter.java
+++ app/src/main/java/co/edu/eafit/teis/sconnection/login/presentor/LoginPresenter.java
@@ -5,34 +5,29 @@ import android.support.v7.app.AppCompatActivity;
 import co.edu.eafit.teis.sconnection.Client.Client;
 import co.edu.eafit.teis.sconnection.Utils.ResponseHandler;
 import co.edu.eafit.teis.sconnection.connection.ClientDao;
+import co.edu.eafit.teis.sconnection.login.view.LoginView;
+import co.edu.eafit.teis.sconnection.search.view.SearchView;
 
 public class LoginPresenter implements ResponseHandler
 {
-    private boolean success;
     private AppCompatActivity ctx;
 
     public LoginPresenter(AppCompatActivity ctx) {
         this.ctx = ctx;
     }
 
-    public boolean tryLogin(String username, String password) {
-
+    public void tryLogin(String username, String password)
+    {
         ClientDao dao = new ClientDao(ctx, this);
         Client client = new Client(true);
         client.setPasswd(password);
         client.setName(username);
         dao.sendLoginRequest(client);
-
-        if(success == true)
-            return true;
-        return false;
     }
 
 
     public void onResponse(String response)
     {
-        String rPassword = null;
-        //extract password
-        //this.rPassword = rPassword;
+        ((LoginView)ctx).SignInSuccess();
     }
 }
--- app/src/main/java/co/edu/eafit/teis/sconnection/login/view/LoginView.java
+++ app/src/main/java/co/edu/eafit/teis/sconnection/login/view/LoginView.java
@@ -4,8 +4,11 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 import co.edu.eafit.teis.sconnection.R;
+import co.edu.eafit.teis.sconnection.login.presentor.LoginPresenter;
+import co.edu.eafit.teis.sconnection.search.view.SearchView;
 import co.edu.eafit.teis.sconnection.signup.view.SignUpView;
 
 public class LoginView extends AppCompatActivity {
@@ -16,8 +19,26 @@ public class LoginView extends AppCompatActivity {
         setContentView(R.layout.login_view);
     }
 
-    public void onButtonClick(View view){
+    public void SignInSuccess()
+    {
+        Intent i = new Intent(this, SearchView.class);
+        startActivity(i);
+    }
 
+    public void SignIn(View view)
+    {
+        EditText et = (EditText) findViewById(R.id.editText4);
+        String password = et.getText().toString();
+        et.setText("");
+        et = (EditText) findViewById(R.id.editText3);
+        String username = et.getText().toString();
+        et.setText("");
+        LoginPresenter lp = new LoginPresenter(this);
+        lp.tryLogin(username,password);
+    }
+
+    public void SignUp(View view)
+    {
         Intent i = new Intent(this, SignUpView.class);
         startActivity(i);
     }
